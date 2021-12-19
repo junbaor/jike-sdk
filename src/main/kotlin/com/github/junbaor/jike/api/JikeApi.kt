652cc4ce0244d265a02b924ee2ebc7ab7136bbb5
junbaor <junbaor@gmail.com>
2021-12-19 12:34:26 +0800

    fix: io.github.junbaor.jike.model.*

--- src/main/kotlin/com/github/junbaor/jike/api/JikeApi.kt
+++ src/main/kotlin/com/github/junbaor/jike/api/JikeApi.kt
@@ -1,15 +1,13 @@
 package com.github.junbaor.jike.api
 
-import feign.Headers
-import feign.RequestLine
-import com.github.junbaor.jike.common.Constants
 import com.github.junbaor.jike.common.Constants.JSON_UTF_8
 import com.github.junbaor.jike.model.*
-import io.github.junbaor.jike.model.*
+import feign.Headers
+import feign.RequestLine
 
 interface JikeApi {
 
-  @Headers(Constants.JSON_UTF_8)
+  @Headers(JSON_UTF_8)
   @RequestLine("POST /1.0/users/loginWithPhoneAndPassword")
   fun loginWithPhoneAndPassword(body: PasswordLoginRequest)
 
